133d934525479bda4894cbf14d54d7fb0db86040
Remmy Zen <remmy.zen@gmail.com>
2022-03-07 14:55:24 +0800

    Start SQL script

--- start_sql.sh
+++ start_sql.sh
@@ -9,23 +9,23 @@ while IFS= read -r line
 do
   if [[ $line == DB_HOST* ]]
   then
-    DB_HOST=$(cut -d "=" -f2- <<< $line)
+    DB_HOST=$(cut -d "=" -f2- <<< $line | tr -d \')
   elif [[ $line == DB_NAME* ]]
   then
-    DB_NAME=$(cut -d "=" -f2- <<< $line)
+    DB_NAME=$(cut -d "=" -f2- <<< $line | tr -d \' )
   elif [[ $line == DB_USER* ]]
   then
-    DB_USER=$(cut -d "=" -f2- <<< $line)
+    DB_USER=$(cut -d "=" -f2- <<< $line | tr -d \' )
   elif [[ $line == DB_PORT* ]]
   then
-    DB_PORT=$(cut -d "=" -f2- <<< $line)
+    DB_PORT=$(cut -d "=" -f2- <<< $line | tr -d \')
   elif [[ $line == DB_PASSWORD* ]]
   then
-    DB_PASSWORD=$(cut -d "=" -f2- <<< $line)
+    DB_PASSWORD=$(cut -d "=" -f2- <<< $line | tr -d \')
   fi
 done < ".env"
 
 URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
-# Run the scripts to start psql.
-psql ${URI} 
+# Run the scripts to insert data.
+psql ${URI}
